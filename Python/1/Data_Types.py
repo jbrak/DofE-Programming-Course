ec836544ee65e9e3374d8eef2e7d747bf990c721
Jbrak <joe@brakoniecki.com>
2020-10-20 21:32:27 +0100

    Wrote Data Types Script

---
+++ Python/1/Data_Types.py
@@ -0,0 +1,19 @@
+#Define the following:
+#Integer -
+#Float -
+#String -
+
+#Fill in weather the following Values are Int, Float or Str.
+132 ==
+0.234 ==
+"129" ==
+999.12 ==
+989768 ==
+"3244.32" ==
+
+#Fix this piece of code:
+favI = input("What is your favorate integer? ")
+favISquared = fav**2
+print(favISquared)
+
+#Re-write the code above so that it does the same for a float:
