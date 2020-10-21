985f197c5d1c87a9f72b1afe84a5525bd8c53bfa
Jbrak <joe@brakoniecki.com>
2020-10-21 08:49:58 +0100

    Updated

---
+++ Python/1/Conditionals.py
@@ -0,0 +1,15 @@
+trainName = "freddy"
+trainType = "diesel"
+trainUse = "passengers"
+trainLength = 9
+trainAge = 50
+trainWorking = True
+
+#Fix this piece of code
+if trainWorking = True:
+    print("Hurray\n The train is in operation!")
+
+#If the train is older than 60 years old then trainworking = False.
+#If the train is inbetween 45 and 60 years old then calculate the amount of
+#Years until it is 60. Print this.
+#If the train is less than 45 years print: "The train is still in operation."
