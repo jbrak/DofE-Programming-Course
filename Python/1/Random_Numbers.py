2ac14045b2ec5ad98bdd927113af977949274b05
Jbrak <joe@brakoniecki.com>
2020-10-21 14:10:21 +0100

    Updated

---
+++ Python/1/Random_Numbers.py
@@ -0,0 +1,9 @@
+#Import random here:
+
+#Print a random Float between 0.0 & 1.0
+
+#Print a random interger between 1 & 10
+
+#Print a random Float between 10 & 71
+
+#Print a random interger between 24 & 98
