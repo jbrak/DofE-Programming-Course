8a4773427999b20d2cb80775d6d05bf70d4e375c
Jbrak <joe@brakoniecki.com>
2020-11-21 15:57:28 +0000

    completed introduction

--- Python/1/Introducion.py
+++ Python/1/Introducion.py
@@ -1,32 +1,37 @@
 #Write A Print Statement
 
-
+print("joe is not abob")
 
 #Multiply 55 and 129 Together. The Answer Should Be 7095.
 
-
+print(55*129)
 
 #Divide 45615438 by 4562. The Answer Should Be 9999.
 
-
+print(45615438/4562)
 
 #Raise 21 to the power of 3. 9261
 
-
+print(21**3)
 
 #Write Variables for your Firstname and your Age.
 
-
+firstname = "jake"
+age = 11
 
 #Write an input statement that asks for your village. Save this to a variable
 #called village
 
-
+village = input("what village do you live in: ")
 
 #Using the age variable, calculate how many years it will be before your 100
 #save this to a variable called yearsTo100
 
-
+yearsTo100 = 100 - age
 
 #Print all of the variables above in the format:
 #Varible Name : Value
+print("firstname : ", firstname)
+print("age : ",age)
+print("village : ",village)
+print("yearsTo100 : ",yearsTo100)
