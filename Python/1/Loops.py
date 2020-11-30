952693e400589682c64b509cc5d92bc583b7f936
Jbrak <joe@brakoniecki.com>
2020-11-30 14:09:20 +0000

    Updated

--- Python/1/Loops.py
+++ Python/1/Loops.py
@@ -1,1 +1,14 @@
-#Write a For loop that prints "monkey" and then the amount of iterations 20 times
+#Write a For loop that prints "monkey" and i 20 times
+
+
+#Write a while loop that does the same thing
+
+
+#Write a For loop that multiplys i by 2 if below 5 and 3 if above 5. THe loop should run 10 times
+
+
+#Write an infinate loop that breaks when x == 100. Print i.
+
+
+#Write a program that prints 10 random floats in between i & i-1
+#So if i = 2 the program will print 10 random numbers between 9 and 10
