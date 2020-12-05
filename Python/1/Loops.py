ee93a794c382fc209f257bc928fcaa8979a31f4c
Jbrak <joe@brakoniecki.com>
2020-12-05 11:29:46 +0000

    COmpleted

--- Python/1/Loops.py
+++ Python/1/Loops.py
@@ -1,14 +1,29 @@
 #Write a For loop that prints "monkey" and i 20 times
-
-
+for i in range(1,21):
+    print("monkey",i)
 #Write a while loop that does the same thing
-
+v = 0
+while v < 21:
+    print("monkey",v)
+    v += 1
 
 #Write a For loop that multiplys i by 2 if below 5 and 3 if above 5. THe loop should run 10 times
+for i in range(1,11):
+    if i < 5:
+        print(i * 2)
+    else:
+        print(i * 3)
 
-
-#Write an infinate loop that breaks when x == 100. Print i.
-
-
+#Write an infinate loop that breaks when x == 100. Print x.
+x = 0
+while True:
+        print(x)
+        if x == 100:
+            break
+        x = x + 1
 #Write a program that prints 10 random floats in between i & i-1
-#So if i = 2 the program will print 10 random numbers between 9 and 10
+#So if i = 2 the program will print 10 random numbers between 1 and 2
+import random
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(random.uniform(i-1, i))
--- Python/1/Random_Numbers.py
+++ Python/1/Random_Numbers.py
@@ -1,9 +1,15 @@
+#What do the following functions do?
+#random - generates ramdom float between 0-1  
+#uniform - generates random float between two numbers
+#randint - generates ramdom integer between two numbers
+
 #Import random here:
-
+from random import uniform, randint
 #Print a random Float between 0.0 & 1.0
-
+print(uniform(0,1))
 #Print a random interger between 1 & 10
-
+print(randint(1,10))
 #Print a random Float between 10 & 71
-
+print(uniform(10,71))
 #Print a random interger between 24 & 98
+print(randint(24,98))
