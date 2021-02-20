e6709607904bcac2937e0358fb344c89516a34dd
Jbrak <joe@brakoniecki.com>
2021-02-20 17:09:13 +0000

    Added dictionary worksheet

---
+++ Python/2/Dictionaries.py
@@ -0,0 +1,20 @@
+#Create a dictionary called profiles. The keys should be usernames and the value passwords.
+
+
+#Print the password for any of your usernames
+
+
+#Print all of the keys for your profiles dictionary
+
+
+#Print all of the Values for your profiles dictionary
+
+
+#Have the reader enter the name of a famous person and then return there birthday.
+#Use this dictionary:
+birthdays = {
+    'Albert Einstein': '03/14/1879',
+    'Benjamin Franklin': '01/17/1706',
+    'Ada Lovelace': '12/10/1815',
+    'Donald Trump': '06/14/1946',
+    'Rowan Atkinson': '01/6/1955'}
