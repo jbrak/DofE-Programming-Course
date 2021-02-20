7cf22f3dc448b5d2db01d5cebfa50a67fd5b333e
Jbrak <joe@brakoniecki.com>
2021-02-20 17:21:20 +0000

    added functions worksheets

---
+++ Python/2/functions.py
@@ -0,0 +1,11 @@
+#Create a function that prints a given perameter multiplied by 43
+
+
+#Create a function that generates a list of 10 random integers between 1 and 50
+
+
+#Create a function that takes 2 perameters:
+#a list of numbers
+#an integer
+#The function should interate through the function looking for the number.
+#If it is there return True. Otherwise return False. 
