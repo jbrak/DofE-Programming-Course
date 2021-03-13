e40e0a15b3e66766b5ccd27d9b97b4a4313029bf
Jbrak <joe@brakoniecki.com>
2021-03-13 13:11:37 +0000

    completed dictionaries

--- Python/2/Dictionaries.py
+++ Python/2/Dictionaries.py
@@ -1,20 +1,23 @@
-#Create a dictionary called profiles. The keys should be usernames and the value passwords.
+#Create a dictionary called Doggies. The keys should be names and the value breeds.
+doggies = {"sampson":"golden retrever", "moses":"poodel", "aaron":"husky", "jake":"labroaoodal"}
 
+#Print the breed for any of their names
+print(doggies["sampson"])
 
-#Print the password for any of your usernames
+#Print all of the keys for your doggies dictionary
+print(list(doggies.keys()))
 
-
-#Print all of the keys for your profiles dictionary
-
-
-#Print all of the Values for your profiles dictionary
-
+#Print all of the Values for your doggies dictionary
+print(list(doggies.values()))
 
 #Have the reader enter the name of a famous person and then return there birthday.
 #Use this dictionary:
 birthdays = {
-    'Albert Einstein': '03/14/1879',
-    'Benjamin Franklin': '01/17/1706',
-    'Ada Lovelace': '12/10/1815',
-    'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955'}
+    'albert einstein': '03/14/1879',
+    'benjamin franklin': '01/17/1706',
+    'ada lovelace': '12/10/1815',
+    'donald trump': '06/14/1946',
+    'rowan atkinson': '01/6/1955'}
+name = input("enter the name of a famous of person:\n").lower()
+
+print(birthdays[name])
