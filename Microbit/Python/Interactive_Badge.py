117973f658f650eb07474a6dbabe493e56f35fb6
Jbrak <joe@brakoniecki.com>
2021-04-03 16:11:17 +0100

    Completed interactive badge

--- Microbit/Python/Interactive_Badge.py
+++ Microbit/Python/Interactive_Badge.py
@@ -1,17 +1,45 @@
-# Docs - https://microbit-micropython.readthedocs.io/en/v2-docs/tutorials/images.htmlhttps://microbit-micropython.readthedocs.io/en/v2-docs/tutorials/images.html
+# Docs - https://microbit-micropython.readthedocs.io/en/v2-docs/tutorials/images.html
 from microbit import *
 
 #Stuff to run once at the beggining
-happy =Image(
+happy = Image(
             "08080:"
             "00800:"
             "80008:"
             "08080:"
             "00800:")
 confused = Image.CONFUSED
-sequence = [happy, confused]
+sequenceA_B = [happy, confused]
+
+sicsorseOpen = Image(
+            "80008:"
+            "08080:"
+            "00800:"
+            "08080:"
+            "88088:")
+strin = Image(
+       "80808:"
+        "08080:"
+        "00800:"
+        "08080:"
+        "88088:")
+
+closedScisors = Image(
+            "00800:"
+            "00800:"
+            "00800:"
+            "08080:"
+            "88088:")
+
+sequenceA = [sicsorseOpen, strin, closedScisors]
+
+sequenceB = [Image.STICKFIGURE,Image.SWORD,Image.SKULL,Image.GHOST,Image.GIRAFFE]
 
 #Stuff to run repeatedly
 while True:
-    if button_a.is_pressed() and button_a.is_pressed():
-        display.show(sequence, delay=500)
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.show(sequenceA_B, delay=500)
+    elif button_a.is_pressed():
+        display.show(sequenceA, delay=500)
+    elif button_b.is_pressed():
+        display.show(sequenceB, delay=700)
